58920a5e5aa631eae73debc24eb1c44d9f2c827c
dekdew <passawit1999@gmail.com>
2018-11-03 04:51:01 +0700

    index.php

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+  <meta charset="UTF-8">
+  <title>เทพเจ้าแห่งโอลิมปัส</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Athiti|Bai+Jamjuree|Kanit" rel="stylesheet"></head>
+
 <body>
-    
+  <section id="header">
+    <div id="black-ov"></div>
+    <div id="header-message">
+      <h1>เทพเจ้าแห่งโอลิมปัส</h1>
+      <h2>สภาเทพแห่งโอลิมปัส เหล่าทวยเทพสูงสุดตามความเชื่อของชาวกรีกโบราณ</h2>
+    </div>
+  </section>
+
+  <section id="gods">
+    <form action="god.php" id="sendGod" method="POST">
+      <ul>
+        <button name="god" type="submit" value="zeus"><p>ซุส</p><img src="img/symbolZeus.svg"></button>
+        <button name="god" type="submit" value="poseidon"><p>โพไซดอน</p><img src="img/symbolPoseidon.svg"></button>
+        <button name="god" type="submit" value="demeter"><p>ดีมิเตอร์</p><img src="img/symbolDemeter.svg"></button>
+        <button name="god" type="submit" value="ares"><p>แอรีส</p><img src="img/symbolAres.svg"></button>
+        <button name="god" type="submit" value="apollo"><p>อพอลโล</p><img src="img/symbolApollo.svg"></button>
+        <button name="god" type="submit" value="artemis"><p>อาร์เทมิส</p><img src="img/symbolArtemis.svg"></button>
+        <button name="god" type="submit" value="hermes"><p>เฮอร์มีส</p><img src="img/symbolHermes.svg"></button>
+        <button name="god" type="submit" value="athena"><p>อธีนา</p><img src="img/symbolAthena.svg"></button>
+        <button name="god" type="submit" value="aphrodite"><p>อโฟร์ได</p><img src="img/symbolAphrodite.svg"></button>
+        <button name="god" type="submit" value="hephaestus"><p>ฮีฟีสทัส</p><img src="img/symbolHephaestus.svg"></button>
+        <button name="god" type="submit" value="dionysus"><p>ไดโอนิซัส</p><img src="img/symbolDionysus.svg"></button>
+        <button name="god" type="submit" value="hera"><p>เฮรา</p><img src="img/symbolHera.svg"></button>
+      </ul>
+    </form>
+  </section>
+
+  <!-- <section id="info">
+  </section> -->
+
 </body>
+
 </html>
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+var data;
 var xmlhttp = new XMLHttpRequest();
-var url = "data.json";
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var gods = JSON.parse(xmlhttp.responseText);
-        myFunction(gods);
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText);
+        
     }
 };
-xmlhttp.open("GET", url, true);
+xmlhttp.open("GET", "data.json", true);
 xmlhttp.send();
-
-function myFunction(gods) {
-    console.log(gods);
+var zeus='zeus';
+function setGod(god) {
+    alert(data.god.greekName);
 }
