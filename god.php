d4008f2d14f4433987fb67d7597c8389928f6274
dekdew <passawit1999@gmail.com>
2018-11-03 15:20:00 +0700

    -

--- god.php
+++ god.php
@@ -9,12 +9,72 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title><?php echo $data->$god->greekName; ?></title>
+	<meta charset="UTF-8">
+	<title>
+		<?php echo $data->$god->greekName; ?>
+	</title>
+	<link rel="stylesheet" href="god.css">
+	<link href="https://fonts.googleapis.com/css?family=Athiti|Bai+Jamjuree|Kanit" rel="stylesheet">
+</head>
+
 </head>
 
 <body>
+	<section id="info">
+		<div id="black-ov"></div>
+		<div class="message">
+			<div class="profile" style="background-image: url('<?php echo $data->$god->img ;?>');">
+			</div>
+			<div class="info">
+				<h1>
+				<?php echo $data->$god->greekName ;?>
+				</h1>
+			</div>
+		</div>
+	</section>
 
+	<section id="gods">
+		<form action="god.php" id="sendGod" method="POST">
+			<ul>
+				<button name="god" type="submit" value="zeus">
+					<p>ซุส</p><img src="img/symbols/symbolZeus.svg">
+				</button>
+				<button name="god" type="submit" value="poseidon">
+					<p>โพไซดอน</p><img src="img/symbols/symbolPoseidon.svg">
+				</button>
+				<button name="god" type="submit" value="demeter">
+					<p>ดีมิเตอร์</p><img src="img/symbols/symbolDemeter.svg">
+				</button>
+				<button name="god" type="submit" value="ares">
+					<p>แอรีส</p><img src="img/symbols/symbolAres.svg">
+				</button>
+				<button name="god" type="submit" value="apollo">
+					<p>อพอลโล</p><img src="img/symbols/symbolApollo.svg">
+				</button>
+				<button name="god" type="submit" value="artemis">
+					<p>อาร์เทมิส</p><img src="img/symbols/symbolArtemis.svg">
+				</button>
+				<button name="god" type="submit" value="hermes">
+					<p>เฮอร์มีส</p><img src="img/symbols/symbolHermes.svg">
+				</button>
+				<button name="god" type="submit" value="athena">
+					<p>อธีนา</p><img src="img/symbols/symbolAthena.svg">
+				</button>
+				<button name="god" type="submit" value="aphrodite">
+					<p>อโฟร์ได</p><img src="img/symbols/symbolAphrodite.svg">
+				</button>
+				<button name="god" type="submit" value="hephaestus">
+					<p>ฮีฟีสทัส</p><img src="img/symbols/symbolHephaestus.svg">
+				</button>
+				<button name="god" type="submit" value="dionysus">
+					<p>ไดโอนิซัส</p><img src="img/symbols/symbolDionysus.svg">
+				</button>
+				<button name="god" type="submit" value="hera">
+					<p>เฮรา</p><img src="img/symbols/symbolHera.svg">
+				</button>
+			</ul>
+		</form>
+	</section>
 </body>
 
 </html>
